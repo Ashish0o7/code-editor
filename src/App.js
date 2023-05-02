7106c1a08302e13a7bddf6f47dc6d82de4fdfcb6
Ashish0o7 <ashishkb0o99@gmail.com>
2023-05-02 13:25:40 +0530

    some pages not loading when hosted, fixed!!

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import Landing from "./components/Landing";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import SavedCodes from "./components/SavedCodes"; // import the new component
+import SavedCodes from "./components/SavedCodes";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FeaturedCodes from "./components/Featured"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // use HashRouter instead of BrowserRouter
+import FeaturedCodes from "./components/Featured";
+
 function App() {
   const [userEmail, setUserEmail] = useState('');
-  const [savedCodes, setSavedCodes] = useState([]); // initialize saved codes as an empty array
+  const [savedCodes, setSavedCodes] = useState([]);
 
   return (
     <div>
-   <Route>
+     
         <Header userEmail={userEmail} />
         <Routes>
           <Route exact path="/" element={<Landing />} />
           <Route path="/login" element={<Login setUserEmail={setUserEmail} />} />
           <Route path="/register" element={<Register setUserEmail={setUserEmail}/>} />
-          <Route path="/saved-codes" element={<SavedCodes savedCodes={savedCodes} />} /> // add the new route
-           <Route path="/featured" element={<FeaturedCodes  />} />
+          <Route path="/saved-codes" element={<SavedCodes savedCodes={savedCodes} />} />
+          <Route path="/featured" element={<FeaturedCodes  />} />
         </Routes>
-</Route>
+   
     </div>
   );
 }
