234754dacbe29aeda7abb1fe9a558be4af0a9ef9
aashish <75740364+Ashish0o7@users.noreply.github.com>
2024-04-09 02:20:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import SavedCodes from "./components/SavedCodes";
 import { useState } from "react";
+import AddQuestion from "./components/add_question";
 import { HashRouter as Router, Routes, Route } from "react-router-dom"; // use HashRouter instead of BrowserRouter
 import FeaturedCodes from "./components/Featured";
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="/register" element={<Register setUserEmail={setUserEmail}/>} />
           <Route path="/saved-codes" element={<SavedCodes/>} />
           <Route path="/featured" element={<FeaturedCodes  />} />
+            <Route path="/add_question" element={<AddQuestion />} />
         </Routes>
 
     </div>
